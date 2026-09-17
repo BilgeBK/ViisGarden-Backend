aab88abef45cfc368203712e0dfe8f74306e788c
BilgeBK <bilgekizgindere@gmail.com>
2026-09-17 19:23:12 +0300

    initial commit

---
+++ src/test/java/com/viisland/ViisGarden/ViisGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viisland.ViisGarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViisGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
